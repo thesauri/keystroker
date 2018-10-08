e3a6866c5512cad76f2e7916f76ced260037bb88
Walter Berggren <walter.berggren@gmail.com>
2018-10-08 20:06:36 +0300

    Remove the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,6 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <nav className="navbar is-dark" role="navigation">
-          <div className="navbar-brand">
-            <h1 className="navbar-item">
-              User authentication study
-            </h1>
-          </div>
-        </nav>
         <Registration />
       </div>
     );
