6a2b2df61619d6c26ded9d9c785cb05bdaffff77
Walter Berggren <walter.berggren@gmail.com>
2018-10-08 14:43:53 +0300

    Add Bulma

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import 'bulma/css/bulma.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
