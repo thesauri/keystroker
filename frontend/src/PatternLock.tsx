af4c668602af2e4aec0f93ad94d30e18d5921fc6
Walter Berggren <walter.berggren@gmail.com>
2018-10-08 21:28:40 +0300

    Track finger position

--- frontend/src/PatternLock.tsx
+++ frontend/src/PatternLock.tsx
@@ -7,6 +7,9 @@ export interface Props {
 interface State {
     width: number;
     height: number;
+    isTouched: boolean;
+    touchX?: number;
+    touchY?: number;
 }
 
 class PatternLock extends React.Component<Props, State> {
@@ -17,8 +20,31 @@ class PatternLock extends React.Component<Props, State> {
         this.canvas = React.createRef();
         this.state = {
             height: 400,
+            isTouched: true,
             width: 350
         }
+        this.touchMove = this.touchMove.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
+        this.touchStart = this.touchStart.bind(this);
+    }
+
+    public componentDidMount() {
+        this.canvas.current!.addEventListener("touchmove", this.touchMove);
+        this.canvas.current!.addEventListener("touchend", this.touchEnd);
+        this.canvas.current!.addEventListener("touchcancel", this.touchEnd);
+        this.canvas.current!.addEventListener("touchstart", this.touchStart);
+    }
+
+    public componentWillUpdate() {
+        // tslint:disable-next-line:no-console
+        console.log(this.state.touchX + " " + this.state.touchY);
+    }
+
+    public componentWillUnmount() {
+        this.canvas.current!.removeEventListener("touchmove", this.touchMove);
+        this.canvas.current!.removeEventListener("touchend", this.touchEnd);
+        this.canvas.current!.addEventListener("touchcancel", this.touchEnd);
+        this.canvas.current!.removeEventListener("touchstart", this.touchStart);
     }
 
     public render() {
@@ -28,6 +54,35 @@ class PatternLock extends React.Component<Props, State> {
                 height={this.state.height}
                 ref={this.canvas} />
         );
+    }
+
+    private touchEnd() {
+        this.setState({
+            isTouched: false
+        });
+    }
+
+    private touchStart() {
+        this.setState({
+            isTouched: true
+        });
+    }
+
+    private touchMove(event: TouchEvent) {
+        if (this.state.isTouched) {
+            const { left, top } = this.canvas.current!.getBoundingClientRect();
+            const touchX = event.targetTouches[0].clientX - left;
+            const touchY = event.targetTouches[0].clientY - top;
+
+            if (touchX >= 0 && touchX < this.state.width &&
+                touchY >= 0 && touchY < this.state.height) {
+                this.setState({
+                    touchX,
+                    touchY
+                });
+            }
+        }
+        event.preventDefault();
     }
 }
 
