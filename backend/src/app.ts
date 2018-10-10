f8f093444bf6728f7bfbfd66cb39c6e17ba8dc50
Walter Berggren <walter.berggren@gmail.com>
2018-10-10 21:05:03 +0300

    Serve build with express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+// Serve static content
+const distPath = __dirname + "/dist";
+app.use(express.static(distPath));
+app.use("/register", express.static(__dirname + "/dist/index.html"));
+app.use("/login", express.static(__dirname + "/dist/index.html"));
+app.use(express.static(distPath));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
