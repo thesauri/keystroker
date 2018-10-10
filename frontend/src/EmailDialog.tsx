7aa5fcacef0d52e821c0efc49d66179452665fbd
Walter Berggren <walter.berggren@gmail.com>
2018-10-10 10:03:10 +0300

    Add registration text to the emaildialog page

--- frontend/src/EmailDialog.tsx
+++ frontend/src/EmailDialog.tsx
@@ -24,7 +24,16 @@ const EmailDialog = observer(() => (
         }}>
         <section className="section">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac placerat lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Thank you for your interest in participating in this study!
+            </p>
+            <p>
+                During the two weeks from Monday October 15 to Sunday October 28, you will be asked to log in to this website four times a day. To make this as simple as possible, you will receive reminders by email with links for logging in.
+            </p>
+            <p>
+                Logging in is done using a password and a pattern that you will assign in the next steps. As the purpose of the study is to compare login methods for mobile phones, please log in using your mobile phone.
+            </p>
+            <p>
+                The total time commitment for this study is estimated to be 1–2 hours. This will be compensated with two cinema tickets. 🍿
             </p>
             <div className="field">
                 <label className="label">
