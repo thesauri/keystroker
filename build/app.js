7f9ca0f0e8f4ba19a38493b15fc36f775fa107c7
Walter Berggren <walter.berggren@gmail.com>
2018-10-10 21:44:07 +0300

    Release

---
+++ build/app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var PORT = 3000;
+var app = express();
+// Serve static content
+var distPath = __dirname + "/dist";
+app.use(express.static(distPath));
+app.use("/register", express.static(__dirname + "/dist/index.html"));
+app.use("/login", express.static(__dirname + "/dist/index.html"));
+app.use(express.static(distPath));
+app.listen(PORT, function () { return console.log("Listening on port " + PORT); });
+//# sourceMappingURL=app.js.map
