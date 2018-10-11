83da942c56de7e45f4e2b2530f6c21ef040910db
Walter Berggren <walter.berggren@gmail.com>
2018-10-11 11:26:44 +0300

    DATABASE -> DATABASE_URL

--- backend/src/model/db.ts
+++ backend/src/model/db.ts
@@ -1,7 +1,7 @@
 import { Pool, QueryResult } from "pg";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production"
 });
 
