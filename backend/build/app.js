18fb9ce2a484f5127302ae84edd1b72526e14deb
Walter Berggren <walter.berggren@gmail.com>
2018-10-11 09:23:43 +0300

    Add hello world

--- backend/build/app.js
+++ backend/build/app.js
@@ -1,11 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 // Serve static content
-var distPath = __dirname + "/dist";
-app.use(express.static(distPath));
+app.use(express.static(__dirname + "/dist"));
 app.use("/register", express.static(__dirname + "/dist/index.html"));
 app.use("/login", express.static(__dirname + "/dist/index.html"));
-app.use(express.static(distPath));
+app.post("/user", function (req, res) {
+    res.send("Hello world!");
+});
 app.listen(PORT, function () { return console.log("Listening on port " + PORT); });
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
-const express = require("express");
+import express = require("express");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Serve static content
-const distPath = __dirname + "/dist";
-app.use(express.static(distPath));
+app.use(express.static(__dirname + "/dist"));
 app.use("/register", express.static(__dirname + "/dist/index.html"));
 app.use("/login", express.static(__dirname + "/dist/index.html"));
-app.use(express.static(distPath));
+
+app.post("/user", (req, res) => {
+    res.send("Hello world!");
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
