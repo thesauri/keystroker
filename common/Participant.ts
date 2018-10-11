da1b022ee3c2dedf551e1fca00025c9e5d24cc8d
Walter Berggren <walter.berggren@gmail.com>
2018-10-11 11:23:43 +0300

    Add forgotten Participant 😭

--- common/Participant.ts
+++ common/Participant.ts
@@ -1,14 +1,16 @@
-export const fromJson = (jsonObject: any): Participant => {
-    hasFields(jsonObject, ["email", "password", "pattern", "toc_accepted"]);
-    return new Participant(jsonObject.email, jsonObject.password, jsonObject.pattern, jsonObject.toc_accepted);
-};
+export const fromJson = (jsonObject: any): Promise<Participant> => (
+    hasFields(jsonObject, ["email", "password", "pattern", "toc_accepted"])
+        .then(() => new Participant(jsonObject.email, jsonObject.password, jsonObject.pattern, jsonObject.toc_accepted))
+);
 
-const hasFields = (object: any, fields: string[]) => {
+const hasFields = (object: any, fields: string[]): Promise<boolean> => {
     for (let field of fields) {
         if (object[field] === undefined) {
-            throw new Error(`Missing field: ${field}`);
+            return Promise.reject(`Missing field: ${field}`);
         }
     }
+
+    return Promise.resolve(true);
 }
 
 export default class Participant {
@@ -25,27 +27,29 @@ export default class Participant {
         this.validate();
     }
 
-    public validate() {
+    public validate(): Promise<boolean> {
         if (this.email.length == 0) {
-            throw new Error("Email cannot be empty");
+            return Promise.reject("Email cannot be empty");
         }
         if (this.email.length > 1024) {
             throw new Error("Email cannot be longer than 1024 characters");
         }
 
         if (this.password.length < 10) {
-            throw new Error("Password must be at least 10 characters long");
+            return Promise.reject("Password must be at least 10 characters long");
         }
         if (this.password.length > 1024) {
-            throw new Error("Password cannot be longer than 1024 characters");
+            return Promise.reject("Password cannot be longer than 1024 characters");
         }
 
         if (this.pattern.length < 2) {
-            throw new Error("The pattern must connect at least 2 dots");
+            return Promise.reject("The pattern must connect at least 2 dots");
         }
 
         if (!this.toc_accepted) {
-            throw new Error("The terms of conditions must be accepted");
+            return Promise.reject("The terms of conditions must be accepted");
         }
+
+        return Promise.resolve(true);
     }
 }
